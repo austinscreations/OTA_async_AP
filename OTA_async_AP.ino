ebe151eb76ed291d8a46479bd5dbf1cf23225c9c
austinscreations <70245481+austinscreations@users.noreply.github.com>
2021-05-10 12:59:06 -0400

    Add files via upload

--- OTA_async_AP.ino
+++ OTA_async_AP.ino
@@ -1,7 +1,3 @@
-//#include <ESP8266WiFi.h>
-//#include <ESPAsyncTCP.h>
-//#include <ESPAsyncWebServer.h>
-
 #if defined(ESP8266)
     #include "ESP8266WiFi.h"
     #include "ESPAsyncTCP.h"
@@ -9,20 +5,24 @@
 #elif defined(ESP32)
     #include "WiFi.h"
     #include "AsyncTCP.h"
-//    #include "Update.h"
+    #include "Update.h"
 //    #include "esp_int_wdt.h"
 //    #include "esp_task_wdt.h"
 #endif
 
 #include "ESPAsyncWebServer.h"
-#include "FS.h"
-
-
+//#include "FS.h"
 
 //#include <WebOTA_async.h>
+//#include "OTA_ASYNC.h"
 #include "config.h"
 
+AsyncWebServer server(80);
 
+long max_sketch_size() {
+  long ret = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+  return ret;
+}
 
 String getID(){
             String id = "";
@@ -37,12 +37,16 @@ String getID(){
 
 String _id = getID();
 
-
 String processor(const String& var){ // change information at bottom page
   if(var == "hostplaceholder"){
     String hosting = "";
     hosting +=  host;
     return hosting;
+  }
+  if(var == "macplaceholder"){
+    String macing = "";
+    macing +=  WiFi.macAddress();
+    return macing;
   }
   if(var == "IDplaceholder"){
     String IDing = "";
@@ -60,8 +64,6 @@ String processor(const String& var){ // change information at bottom page
   }
   return String();
 }
-
-AsyncWebServer server(80);
 
 void setup_wifi() {
   delay(10);
@@ -82,11 +84,22 @@ void setup_wifi() {
 
   Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.print("MAC address: ");
+  Serial.println(WiFi.macAddress());
 }
 
+const int ledPin =  LED_BUILTIN;// the number of the LED pin
+int ledState = LOW;             // ledState used to set the LED
+unsigned long previousMillis = 0;        // will store last time LED was updated
+const long interval = 1000;           // interval at which to blink (milliseconds)
+
+
 void setup(void) {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH); // turn led off
+  
   Serial.begin(115200);
   WiFi.hostname(host);
   setup_wifi();
@@ -103,14 +116,74 @@ void setup(void) {
     request->send_P(200, "text/html", update_html, processor );
   });
 
-  server.begin();
+  // Handling uploading firmware file
+//  server.on("/update", HTTP_POST, [&](AsyncWebServerRequest *request) {
+//    request->send_P(200, "text/plain", (Update.hasError()) ? "Update: fail\n" : "Update: OK!\n");
+//    delay(500);
+//    ESP.restart();
+//  },[&](AsyncWebServerRequest *request) {
+//    
+//HTTPUpload& upload = server.upload();
+//
+//    if (upload.status == UPLOAD_FILE_START) {
+//      Serial.printf("Firmware update initiated: %s\r\n", upload.filename.c_str());
+//
+//      //uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+//      uint32_t maxSketchSpace = max_sketch_size();
+//
+//      if (!Update.begin(maxSketchSpace)) { //start with max available size
+//        Update.printError(Serial);
+//      }
+//    } else if (upload.status == UPLOAD_FILE_WRITE) {
+//      /* flashing firmware to ESP*/
+//      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+//        Update.printError(Serial);
+//      }
+//
+//      // Store the next milestone to output
+//      uint16_t chunk_size  = 51200;
+//      static uint32_t next = 51200;
+//
+//      // Check if we need to output a milestone (100k 200k 300k)
+//      if (upload.totalSize >= next) {
+//        Serial.printf("%dk ", next / 1024);
+//        next += chunk_size;
+//      }
+//    } else if (upload.status == UPLOAD_FILE_END) {
+//      if (Update.end(true)) { //true to set the size to the current progress
+//        Serial.printf("\r\nFirmware update successful: %u bytes\r\nRebooting...\r\n", upload.totalSize);
+//      } else {
+//        Update.printError(Serial);
+//      }
+//    }
+//  });
 
-//  webota.init(8888, "/update");
-  Serial.println("HTTP server started");
+  server.begin();
+  
+//  OTA_ASYNC.begin(&server);    // Start OTA
+
+//  Serial.println("HTTP server started")
+
+
+
+
+;
 }
 
 void loop() {
 
-  
+//unsigned long currentMillis = millis();
+//  if (currentMillis - previousMillis >= interval) {
+//    previousMillis = currentMillis;
+//    if (ledState == LOW) {
+//      ledState = HIGH;
+//    } else {
+//      ledState = LOW;
+//    }
+//    digitalWrite(ledPin, ledState);
+//  }
+
+
+//OTA_ASYNC.loop();
 //    webota.handle();
 }
--- config.h
+++ config.h
@@ -7,8 +7,6 @@ const char* host     = "ESP-OTA";
 
 
 
-
-
 /* Webpage HTML */ 
 
 #ifndef Webpage_h
@@ -16,7 +14,8 @@ const char* host     = "ESP-OTA";
 
 
 const char home_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
+<!DOCTYPE HTML>
+<html>
 <head>
 <title>ESP Web Home</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -27,16 +26,20 @@ const char home_html[] PROGMEM = R"rawliteral(
     font-size: 3.0rem;
     color: #5900b8;
     }
-
+    
     .name-host {
     font-size: 16px;
-    margin-bottom: 20px;
+    margin-bottom: 5px;
     }
     .name-text {
     font-size: 20px;
     font-weight: bold;
     }
-
+    .name-mac {
+    font-size: 16px;
+    margin-bottom: 20px;
+    }
+    
    .menu-buttons{
      margin-bottom: 40px;
     }
@@ -89,12 +92,8 @@ const char home_html[] PROGMEM = R"rawliteral(
 </head>
 <body>
    <h2>ESP Web Home</h2>
-
-   <div class = "name-host">
-    <span class = "name-text">Host Name:</name-text></span>
-    <span class = "host">%hostplaceholder%</host></span>
-   </div>
    
+  
    <div class = "menu-buttons">
    <div class = "button button1">
      <button1>Home</button>
@@ -102,6 +101,16 @@ const char home_html[] PROGMEM = R"rawliteral(
     <div class = "button button2">
      <button2 onclick="location.href='/update'">Update</button>
     </div>
+   </div>
+   
+    <div class = "name-host">
+    <span class = "name-text">Host Name:</name-text></span>
+    <span class = "host">%hostplaceholder%</host></span>
+   </div>
+   
+   <div class = "name-mac">
+    <span class = "name-text">MAC Address:</name-text></span>
+    <span class = "host">%macplaceholder%</host></span>
    </div>
    
     <div class="columns mt-4">
@@ -119,7 +128,6 @@ const char home_html[] PROGMEM = R"rawliteral(
 </script>
 </body>
 </html>
-
 )rawliteral";
 
 
@@ -137,8 +145,7 @@ const char update_html[] PROGMEM = R"rawliteral(
     }
    .menu-buttons{
      margin-bottom: 40px;
-    }
-    
+    }    
    .button {
      border: 2px solid;
      border-radius: .4rem;
@@ -184,8 +191,14 @@ const char update_html[] PROGMEM = R"rawliteral(
     font-size: 14px;
     padding: 4px;
    }
-   .flash-button{
-     border: 2px solid;
+     
+    input[type="submit"] {
+     background-color: #000000;
+     border-color: #000000;
+     color: white;
+     font-size: 16px;
+     font-weight: bold;
+     border: 3px solid;
      border-radius: .4rem;
      font-weight: bold;
      padding: 4px 8px;
@@ -193,22 +206,22 @@ const char update_html[] PROGMEM = R"rawliteral(
      text-decoration: none;
      display: inline-block;
      font-size: 16px;
-     margin: 10px 4px 4px 4px;
+     margin: 10px 5px 4px 4px;
      cursor: pointer;
-    }
-    
-    .button-flash {
-     background-color: #000000;
-     border-color: #000000;
-     color: white;
-     } 
-    
-    .button-flash:hover {
-     background-color: #ffffff;
-     border-color: #5900b8;
-     color: #5900b8;
      }
-
+    input[type="submit"]:hover {
+     background-color: #ffffff;
+     border-color: #ffffff;
+     color: #5900b8;
+     border: 3px solid;
+     border-radius: .4rem;
+     font-weight: bold;
+     font-size: 16px;
+     cursor: pointer;
+     
+     }
+   
+   
    
    .mt-4 {
     margin-top: 40px;
@@ -235,38 +248,89 @@ const char update_html[] PROGMEM = R"rawliteral(
    
    <div class = "menu-buttons">
     <div class = "button button1">
-      <button1 onclick="location.href='/home'">Home</button>
+      <button1 onclick="location.href='/home'">Home</button1>
     </div>
     <div class = "button button2">
-      <button2>Update</button>
+      <button2>Update</button2>
     </div>
    </div>
    
    <div class="col-12 mt-3 p-centered">
+  
      <div class="col-3 col-sm-9 col-md-6 p-centered">
        <label class="label label2 mr-2">Upload Firmware Here</label>
-     <div class="form-group pt-2 mt-3"><input type="file" accept=".bin" class="form-input file-input">
-     </div>
-
-     <div class = "flash-button button-flash">
-      <button-flash> FLASH </button>
+    <div class="form-group pt-2 mt-3">
+    <form method="POST" action="#" enctype="multipart/form-data" id="upload_form">
+    <input type="file" name="update" id="file" accept=".bin" class="form-input file-input">
+    <input type="submit" value="FLASH">
+    </form>
      </div>
      
-     <div class="columns mt-4">
-       <div class="col-12 text-center">
-        <span class="label label-rounded mr-2">%IDplaceholder%</span> 
-         -
-        <span class="label label-rounded label-primary ml-2">%processorplaceholder%</span>
-       </div>
-      </div> 
+    <div id="prg_wrap" style="border: 0px solid; width: 100%;">
+     <div id="prg" style="text-shadow: 2px 2px 3px black; padding: 5px 0; display: none; border: 1px solid       #008aff; background: #002180; text-align: center; color: white;">
+     </div>
     </div>
+         
+    <div class="columns mt-4">
+      <div class="col-12 text-center">
+       <span class="label label-rounded mr-2">%IDplaceholder%</span> 
+       -
+       <span class="label label-rounded label-primary ml-2">%processorplaceholder%</span>
+     </div>
+    </div> 
+  </div>
   
  
 
 
+<script>
+var domReady = function(callback) {
+  document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
+};
+
+domReady(function() {
+  var myform = document.getElementById('upload_form');
+  var filez  = document.getElementById('file');
+
+  myform.onsubmit = function(event) {
+    event.preventDefault();
+
+    var formData = new FormData();
+    var file     = filez.files[0];
+
+    if (!file) { return false; }
+
+    formData.append("files", file, file.name);
+
+    var xhr = new XMLHttpRequest();
+    xhr.upload.addEventListener("progress", function(evt) {
+      if (evt.lengthComputable) {
+        var per = Math.round((evt.loaded / evt.total) * 100);
+        var prg = document.getElementById('prg');
+
+        prg.innerHTML     = per + "%"
+        prg.style.width   = per + "%"
+        prg.style.display = "block"
+      }
+    }, false);
+    xhr.open('POST', location.href, true);
+
+    // Set up a handler for when the request finishes.
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        //alert('Success');
+      } else {
+        //alert('An error occurred!');
+      }
+    };
+
+    xhr.send(formData);
+   }
+});
 </script>
 </body>
 </html>
+
 
 )rawliteral";
 
